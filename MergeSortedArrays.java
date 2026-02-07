dc474d162da080149b73cf2130b1a52dac44463a
juicetein <justin.lao14@gmail.com>
2026-02-06 19:13:51 -0800

    Add merge sorted arrays implementation
    Implement a method to merge two sorted arrays into one sorted array.

---
+++ MergeSortedArrays.java
@@ -0,0 +1,40 @@
+public class MergeSortedArrays {
+
+    /*
+     * we take two sorted arrays and merge them into
+     * one sorted array
+     */
+    public static int[] merge(int[] A, int[] B) {
+
+        int[] result = new int[A.length + B.length];
+
+        int i = 0; // pointer for array A
+        int j = 0; // pointer for array B
+        int k = 0; // pointer for result array
+
+        while (i < A.length && j < B.length) {
+            if (A[i] <= B[j]) {
+                result[k] = A[i];
+                i++;
+            } else {
+                result[k] = B[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < A.length) {
+            result[k] = A[i];
+            i++;
+            k++;
+        }
+
+        while (j < B.length) {
+            result[k] = B[j];
+            j++;
+            k++;
+        }
+
+        return result;
+    }
+}
